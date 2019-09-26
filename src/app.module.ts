79f7379f370680bde112f011f3e2055e447ebc20
John Gethoefer <john.gethoefer@melissadata.com>
2019-09-26 15:46:37 -0700

    updates with static entity configuration for resolving errors when running in :dev mode

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SubmissionModule } from './submissions/submission.module';
+import { Figure } from './entity/figure';
+import { Submission } from './entity/submission';
 
 @Module({
   imports: [
     SubmissionModule,
     TypeOrmModule.forRoot({
+      name: 'default',
       type: 'sqlite',
       database: 'fingest.db',
-      entities: [__dirname + '/entity/*.ts'],
+      entities: [Figure, Submission],
       synchronize: true,
     }),
   ],
